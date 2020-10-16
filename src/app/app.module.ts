bcd2ac68b9afd8ff3124b15b16ab9f17ee7a7060
Garrett Sloup <garrettsloup@MacBook-Pro.local>
2020-10-16 14:11:41 -0500

    adds firebase to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { ProjectsComponent } from './projects/projects.component';
 import { BugsComponent } from './bugs/bugs.component';
 import { MaterialModule } from './materialModule';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -20,7 +24,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     MaterialModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBKl7FBSs4hMiy-cxzdUB-mHcgwp_KGgHE",
+    authDomain: "bug-tracker-angular.firebaseapp.com",
+    databaseURL: "https://bug-tracker-angular.firebaseio.com",
+    projectId: "bug-tracker-angular",
+    storageBucket: "bug-tracker-angular.appspot.com",
+    messagingSenderId: "976737204531",
+    appId: "1:976737204531:web:870891098e7cef1187e646"
+  }
 };
 
 /*
