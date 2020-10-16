e821ec9fe4b59444c580969a15b564778b8ef27f
Garrett Sloup <garrettsloup@MacBook-Pro.local>
2020-10-16 10:08:36 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { BugsComponent } from './bugs/bugs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProjectsComponent,
+    BugsComponent
   ],
   imports: [
     BrowserModule,
