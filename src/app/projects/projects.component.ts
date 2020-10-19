5543c55ae77dbcf8a71b5384ef354a3738d7cbe6
Garrett Sloup <garrettsloup@MacBook-Pro.local>
2020-10-18 20:59:16 -0500

    adds the removeProj() and state management

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Subject } from 'rxjs';
 import { ProjectsService } from '../services/projects.service';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-projects',
@@ -10,16 +14,31 @@ export class ProjectsComponent implements OnInit {
 
   title: string = '';
   description: string = '';
-  projects: Array<any> = [];
+  projects: Array<Object> = [];
 
-  constructor(private projService: ProjectsService) { }
+  userId$ = new Subject<string>();
+  projects$ = this.userId$.pipe(
+    switchMap(uid => {
+      return this.afs.collection('projects', ref => ref.where('userId', '==', uid)).valueChanges({idField: 'id'})
+    })
+  )
+
+  constructor(private projectService: ProjectsService, private afs: AngularFirestore, private auth: AngularFireAuth) { 
+    this.auth.user.subscribe(v => {
+      this.userId$.next( v ? v.uid : null);
+    });
+    this.projects$.subscribe(val => this.projects = val);
+  }
 
   ngOnInit(): void {
   }
 
   addProject(){
-    this.projService.addProject(this.title, this.description);
-    console.log('addProject() runs in component')
+    this.projectService.addProject(this.title, this.description);
+    console.log('addProject() runs in component');
+  }
+  removeProject(id){
+    this.projectService.removeProject(id);
   }
 
 }
