9288c27bfa4c8b8b8a7a4c2f59657d1175d1d2d0
Garrett Sloup <garrettsloup@MacBook-Pro.local>
2020-10-27 14:04:21 -0500

    connect template inputs to controller

--- src/app/bugs/bugs.component.ts
+++ src/app/bugs/bugs.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BugsComponent implements OnInit {
 
+  title: string;
+  difficulty: string;
+  description: string;
+  status: string;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addBug() {
+    // ADD FUNCTIONALITY TO THE PROJECTS SERVICE TO SEND THIS INFO TO THE DB
+    console.log(this.title);
+    console.log(this.difficulty);
+    console.log(this.description);
+    console.log(this.status);
+    
+    // Clear input data after new bug is added
+    this.title = '';
+    this.difficulty = ''; // may need to set options to ull
+    this.description = '';
+    this.status = '';
+    
+
+  }
 }
